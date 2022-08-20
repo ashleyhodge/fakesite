ede7493300962e852280498396279a74167e15d2
ashleyhodge <ashhodge@comcast.net>
2022-08-20 17:49:01 -0400

    add and functionality to video carousel

--- components/Home/VideoSection/index.js
+++ components/Home/VideoSection/index.js
@@ -6,13 +6,13 @@ import ReactPlayer from 'react-player'
 import videos from '/videos.json'
 import { GrPrevious } from 'react-icons';
 import { Carousel } from 'react-bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function Video() {
-
-
   const [hasWindow, setHasWindow] = useState(false);
+  const [isActive, setIsActive] = useState(false)
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -20,28 +20,41 @@ function Video() {
     }
   }, []);
 
+  // Remove HTML tags from description in videos.json
   function removeTags(str) {
     if ((str===null) || (str===''))
         return false;
     else
         str = str.toString();
           
-    // Regular expression to identify HTML tags in 
-    // the input string. Replacing the identified 
-    // HTML tag with a null string.
+    // Regular expression to identify HTML tags in the input string. Replacing the identified HTML tag with a null string.
     return str.replace( /(<([^>]+)>)/ig, '');
-}
-
+  }
+  // On click set style change as active
+  const handleClick = () => {
+    setIsActive(current => !current)
+  }
+// on arrow click reset style to show background and play button
+  const arrowClick = () => {
+    if(isActive === true) {
+      setIsActive(false)
+    }
+  }
 
   return (
     <div>
-      <Carousel nextIcon={ <span></span>} prevIcon={ <span></span>}>
-        {videos.videos?.map((video) => {
-          return (
-            <Carousel.Item key={video.id} 
-            interval={1000000000}
-            >
-              <div className="grid grid-cols-2 gap-4  mb-[40px]">
+      <div>
+        {/* Use Bootstrap React Carousel component to rotate through videos.json */}
+        <Carousel nextIcon={<img src='/images/next.png' onClick={arrowClick} className='absolute right-0 w-[30px]'/>} prevIcon={ <img src='/images/back.png' className='absolute left-0 w-[30px]' onClick={arrowClick}/>} indicators={false}>
+          {/* Use map method to render all videos from .json file onto page */}
+          {videos.videos?.map((video) => {
+            return (
+              <Carousel.Item key={video.id} 
+              // set interval to outrageous number to prevent carousel from moving on its own 
+              interval={100000000}>
+                
+                <div className="grid grid-cols-2 gap-4  mb-[40px]">
+                {/* Title and description section */}
                   <div className='mt-[40px] mb-[200px]'>
                     <h2 className="text-[36px] ml-[70px] text-blackish">
                       {video.title}
@@ -50,31 +63,33 @@ function Video() {
                       {removeTags(video.description)}
                     </p>
                   </div>
-                <div className='flexbox justify-center items-center'>
-                  <img src='/images/playbtn.png' className='absolute right-0 mt-[120px] mx-[300px] z-20 hover:w-[130px] hover:h-[130px]' />
-                  <div className='absolute right-0 mt-[39px] transition-opacity py-[180px] px-[322px] z-10 bg-gradient-to-b from-redish to-[#FFB1B5] opacity-80 rounded-lg hover:opacity-0 hover:duration-300 duration-[8000ms] mr-[27px] border-[2px] border-redish shadow-lg shadow-white'/>
-                <div className='absolute right-0 mr-[30px] mt-[40px]'>
-                  {hasWindow && <ReactPlayer
-                    url={video.url}
-                    controls={false}
-                    vimeoconfig={{ iframeParams: { fullscreen: 0 } }}
+                {/* Play button and background gradient overlay */}
+                  <div className='flexbox justify-center items-center'>
+                    {/* Change z-index to 0 on click to send bg & img to back and allow video play */}
+                    <div className='cursor-pointer z-20' style={{zIndex: isActive ? 0 : '', opacity: isActive ? 0 : ''}} onClick={handleClick}>
+                      <img src='/images/playbtn.png' alt='play' className='absolute right-0 mt-[140px] mx-[300px] z-20' />
 
-                  />}
+                      <div className='absolute right-0 mt-[50px] transition-opacity py-[170px] px-[302px] z-10 bg-gradient-to-b from-redish to-[#FFB1B5] opacity-90 rounded-lg  duration-[8000ms] mr-[67px] border-[1px] border-blackish shadow-lg shadow-blackish' />
+                      
+                    </div>
+                    {/* Play video through React-Player */}
+                    <div className='absolute right-0 mr-[70px] mt-[40px]' style={{zIndex: isActive ? 20 : ''}} onClick={handleClick}>
+                    {hasWindow && <ReactPlayer
+                      url={video.url}
+                      controls={true}
+                      vimeoconfig={{ iframeParams: { fullscreen: 0 } }}
+                      width={'600px'}/>}
+                    </div>
+                  </div>
                 </div>
-                </div>
-                
-                
-              </div>
-            </Carousel.Item>
-          )
-        })}
-      </Carousel>
+              </Carousel.Item>
+            )
+          })}
+        </Carousel>
+      </div>
     </div>
   )
 }
-
-
-
 
 
 export default Video;
